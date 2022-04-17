c8e48b9be57921e43cc94c4f1a3385d2d232a1f9
DEVSFER\ferna <fernando.chaves16@hotmail.com>
2022-04-17 10:36:04 -0300

    feat: updates in header

--- shared/header/index.tsx
+++ shared/header/index.tsx
@@ -1,14 +1,27 @@
-import StyledHeader from './styles';
+import { HeaderWrapper, HeaderInnerWrapper, HeaderLink } from './styles';
 import { CenterWrapper } from '@/shared';
+
+const headerItems = [
+  { id: 1, name: 'Home' },
+  { id: 2, name: 'Topics' },
+  { id: 3, name: 'About' },
+  { id: 4, name: 'Contact' },
+];
 
 const Header = () => {
   return (
-    <StyledHeader>
+    <HeaderWrapper>
       <CenterWrapper>
-        <h1>Header</h1>
-        <p>teste</p>
+        <HeaderInnerWrapper>
+          <h1>Header</h1>
+          {headerItems.map((item, index) => (
+            <HeaderLink href='/' key={index}>
+              {item.name}
+            </HeaderLink>
+          ))}
+        </HeaderInnerWrapper>
       </CenterWrapper>
-    </StyledHeader>
+    </HeaderWrapper>
   );
 };
 
--- shared/header/styles.ts
+++ shared/header/styles.ts
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
+import Link from 'next/link';
 
-const StyledHeader = styled.header`
-  background-color: #282c34;
-  
+export const HeaderWrapper = styled.header`
+  background-color: rgba(255, 255, 255, 1);
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
+  padding-top: 1.25rem;
+  padding-bottom: 1.25rem;
 `;
 
-export default StyledHeader;
+export const HeaderInnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+`
+
+export const HeaderLink = styled(Link)`
+  color: black;
+  background: blue;
+  font-weight: 600;
+`
