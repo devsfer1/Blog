c76a1d89849c0a10394be9c7663a782b91b78531
devsfer1 <fernando.chaves16@hotmail.com>
2022-09-14 19:51:08 -0300

    styles: home featured structure

--- layout/HomeFeatured/index.tsx
+++ layout/HomeFeatured/index.tsx
@@ -1,13 +1,30 @@
-import { Container, FeaturedItem, ItemOne, ItemTwo, ItemThree, ItemFour } from './styles'
+import { Container, FeaturedItem, ItemOne, ItemTwo, ItemThree, ItemFour, LargePostImageWrapper, MediumPostImageWrapper } from './styles'
+import Image from 'next/image';
 
 
 const HomeFeatured = () => {
   return (
     <Container>
-      <ItemOne />
-      <ItemTwo />
-      <ItemThree />
-      <ItemFour />
+      <ItemOne>
+        <LargePostImageWrapper>
+          <Image src={'/post_1.jpg'} layout='fill' objectFit='cover' />
+        </LargePostImageWrapper>
+      </ItemOne>
+      <ItemTwo>
+        <MediumPostImageWrapper>
+            <Image src={'/post_1.jpg'} layout='fill' objectFit='cover' />
+        </MediumPostImageWrapper>
+      </ItemTwo>
+      <ItemThree>
+        <MediumPostImageWrapper>
+            <Image src={'/post_1.jpg'} layout='fill' objectFit='cover' />
+        </MediumPostImageWrapper>
+      </ItemThree>
+      <ItemFour>
+        <MediumPostImageWrapper>
+            <Image src={'/post_1.jpg'} layout='fill' objectFit='cover' />
+        </MediumPostImageWrapper>
+      </ItemFour>
      
     </Container>
   )
--- layout/HomeFeatured/styles.ts
+++ layout/HomeFeatured/styles.ts
@@ -3,31 +3,46 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  grid-template-rows: repeat(3,200px);
-  gap: 20px;
+  grid-template-rows: repeat(3,210px);
+  gap: 60px;
   width: 100%;
+  margin-top: 120px;
 `
 
 export const FeaturedItem = styled.div`
-  background-color: red;
+  
   width: 200px;
   height: 300px;
+  border: 1px solid black;
 `
 
 export const ItemOne = styled.div`
-  grid-column: 1 / 5;
-  background-color: black;
+  border: 1px solid black;
+  grid-column: 1 / 3;
+  grid-row: 1 / 4;
+  
 `
 export const ItemTwo = styled.div`
-  grid-column: 1 / 3;
-  grid-row: 2 / 4;
-  background-color: black;
+  grid-column: 3 / 5;
+  border: 1px solid black;
 `
 export const ItemThree = styled.div`
   grid-column: 3 / 5;
-  background-color: black;
+  border: 1px solid black;
 `
 export const ItemFour = styled.div`
-  background-color: black;
+  border: 1px solid black;
+  grid-column: 3 / 5;
+`
 
+export const LargePostImageWrapper = styled.div`
+  width: 100%;
+  height: 55%;
+  position: relative;
+`
+
+export const MediumPostImageWrapper = styled.div`
+  width: 40%;
+  height: 100%;
+  position: relative;
 `
